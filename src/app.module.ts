d1e283ad32a85b103d8f0f545a2ee2522532b7db
Sam Magura <srmagura@gmail.com>
2023-05-03 13:31:07 -0400

    get it to work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,33 @@
+/* eslint-disable */
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 
 import { OrgResolver } from './OrgResolver';
 
+const NotWorkingVersion = GraphQLModule.forRoot<ApolloDriverConfig>({
+  driver: ApolloDriver,
+  autoSchemaFile: 'schema.gql',
+  subscriptions: {
+    'graphql-ws': true,
+  },
+});
+
+const WorkingVersion = GraphQLModule.forRoot<ApolloDriverConfig>({
+  driver: ApolloDriver,
+  autoSchemaFile: 'schema.gql',
+  subscriptions: {
+    subscriptions: {
+      'graphql-ws': true,
+    },
+  } as any,
+});
+
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: 'schema.gql',
-      // installSubscriptionHandlers: true,
-      subscriptions: {
-        'graphql-ws': true,
-      },
-    }),
+    // Uncomment the "version" that you want to use:
+    NotWorkingVersion,
+    // WorkingVersion,
   ],
   providers: [OrgResolver],
 })
